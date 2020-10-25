16d0cf046db27960a1b09742f338d7cd937e9840
hideyorimurakami <71474250+hideyorimurakami@users.noreply.github.com>
2020-10-25 15:18:12 +0900

    Add files via upload

---
+++ janken/src/main/java/com/example/demo/controllers/JankenController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.models.Hantei;
+import com.example.demo.models.JankenRepository;
+import com.example.demo.models.Jankenuser;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+@Controller
+public class JankenController {
+
+	private final JankenRepository rep;
+
+	@GetMapping("/")
+	public String index(Model model,HttpSession s, @ModelAttribute Jankenuser jankenuser,@ModelAttribute Hantei hantei) {
+		String sessionId = s.getId();
+		jankenuser.setSessionId(sessionId);
+		rep.save(jankenuser);
+		s.setAttribute("user", jankenuser);
+		s.setAttribute("hantei", hantei);
+		model.addAttribute("msg","選んでください！");
+		return "janken";
+	}
+
+	@PostMapping("/janken")
+	public String test(@RequestParam String te,HttpSession s,Model model) {
+		Jankenuser jankenuser = (Jankenuser)s.getAttribute("user");
+		Hantei hantei = (Hantei)s.getAttribute("hantei");
+		int you = Integer.parseInt(te);
+		model.addAttribute("msg", hantei.judge(you));
+		model.addAttribute("cpu",hantei.getCpu());
+		model.addAttribute("you",hantei.getYou());
+		jankenuser.setRound(hantei.getRound());;
+		jankenuser.setWin(hantei.getWin());
+		jankenuser.setLose(hantei.getLose());
+		jankenuser.setDraw(hantei.getDraw());
+		rep.save(jankenuser);
+		model.addAttribute("jankenuser", jankenuser);
+		return "janken";
+	}
+}
---
+++ janken/src/main/java/com/example/demo/models/Jankenuser.java
@@ -0,0 +1,33 @@
+package com.example.demo.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Jankenuser {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "sessionId")
+	private String sessionId;
+
+	@Column(name = "round")
+	private int round;
+
+	@Column(name = "win")
+	private int win;
+
+	@Column(name = "lose")
+	private int lose;
+
+	@Column(name = "draw")
+	private int draw;
+}
